aeab0f974775e6492afcc2f522841274fae55dde
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-01-02 19:50:32 -0500

    add reminder of why we have to do specific plugin code

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ import createMDX from '@next/mdx'
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
- 
+
+// We can't just import them and use that due to those not being serializable.
 const withMDX = createMDX({
   options: {
     remarkPlugins: [
